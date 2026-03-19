537c990445044d8fb09452d455c8f84e6f55a25e
yigiuwole <3251013822@qq.com>
2026-03-19 16:23:22 +0800

    静态资源引入补充（之前忘记在main.js文件中引入初始化样式文件）

--- src/main.js
+++ src/main.js
@@ -5,6 +5,9 @@ import App from './App.vue'
 import router from './router'
 import { getCategoryApi } from '@/apis/testApi'
 
+//引入初始化样式文件
+import '@/styles/common.scss'
+
 const app = createApp(App)
 
 //测试接口函数
