e7f9031fbadc622b680f332183a76736c0032285
yigiuwole <3251013822@qq.com>
2026-03-24 16:47:44 +0800

    home页新鲜好物和人气推荐模块的panel组件实现

--- src/apis/home.js
+++ src/apis/home.js
@@ -5,3 +5,25 @@ export function getBannerAPI() {
     url: '/home/banner'
   })
 }
+
+/**
+ *@description :获取新鲜好物
+ *@param {*}
+ *@return {*}
+ */
+export function findNewAPI() {
+  return httpInstance({
+    url: '/home/new'
+  })
+}
+
+/**
+ *@description :获取人气推荐
+ * @param {*}
+ * @return {*}
+ */
+export function findHotAPI() {
+  return httpInstance({
+    url: '/home/hot'
+  })
+}
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -4,7 +4,6 @@ import HomeBanner from './components/HomeBanner.vue'
 import HomeNew from './components/HomeNew.vue'
 import HomeHot from './components/HomeHot.vue'
 import HomeProduct from './components/HomeProduct.vue'
-import HomePanel from './components/HomePanel.vue'
 </script>
 
 <template>
@@ -14,15 +13,5 @@ import HomePanel from './components/HomePanel.vue'
   </div>
   <HomeNew />
   <HomeHot />
-  <HomePanel title="新鲜好物" subtitle="新鲜好物 好多商品">
-    <div>
-      我是新鲜好物的插槽
-    </div>
-  </HomePanel>
-  <HomePanel title="人气推荐" subtitle="人气推荐 好多商品">
-    <div>
-      我是人气推荐的插槽
-    </div>
-  </HomePanel>
   <HomeProduct />
 </template>
