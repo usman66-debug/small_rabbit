0d2b8aadc2bf0e27bf5ece10c0a71d1f82e987b4
yigiuwole <3251013822@qq.com>
2026-03-24 10:20:21 +0800

    home页左侧导航栏功能制作

--- src/main.js
+++ src/main.js
@@ -3,17 +3,13 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-import { getCategoryApi } from '@/apis/testApi'
+
 
 //引入初始化样式文件
 import '@/styles/common.scss'
 
 const app = createApp(App)
 
-//测试接口函数
-// getCategoryApi().then(res => {
-//   console.log(res);
-// })
 
 app.use(createPinia())
 app.use(router)
