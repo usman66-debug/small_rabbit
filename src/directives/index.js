9825cd731822e128e8c9430c34a98c8e179f4943
yigiuwole <3251013822@qq.com>
2026-03-26 12:54:00 +0800

    HomeProduct页GoodsItem小卡片组件封装

--- src/directives/index.js
+++ src/directives/index.js
@@ -6,10 +6,8 @@ export const lazyPlugin = {
       mounted(el, binding) {
         //el 为绑定的标签
         //binding  binding.value为指令等号后面的值
-        console.log(el, binding.value);
         const { stop } = useIntersectionObserver(el,
           ([{ isIntersecting }]) => {
-            console.log(isIntersecting);
             if (isIntersecting) {
               el.src = binding.value
               stop()
