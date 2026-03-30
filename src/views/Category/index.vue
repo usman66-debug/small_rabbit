cd4c151811839429677f8586e2cf53b786a0b2ed
yigiuwole <3251013822@qq.com>
2026-03-30 17:44:52 +0800

    激活状态显示以及分类列表渲染

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -4,6 +4,7 @@ import { useRoute } from 'vue-router'
 import { ref } from 'vue'
 import { onMounted } from 'vue'
 import { getBannerAPI } from '@/apis/home';
+import GoodsItem from '@/views/Home/components/GoodsItem.vue'
 
 
 //获取数据
@@ -44,6 +45,26 @@ onMounted(() => getBanner())
             <img :src="item.imgUrl" alt="">
           </el-carousel-item>
         </el-carousel>
+      </div>
+      <!-- 主体部分 -->
+      <div class="sub-list">
+        <h3>全部分类</h3>
+        <ul>
+          <li v-for="i in categoryData.children" :key="i.id">
+            <RouterLink to="/">
+              <img :src="i.picture" />
+              <p>{{ i.name }}</p>
+            </RouterLink>
+          </li>
+        </ul>
+      </div>
+      <div class="ref-goods" v-for="item in categoryData.children" :key="item.id">
+        <div class="head">
+          <h3>- {{ item.name }}-</h3>
+        </div>
+        <div class="body">
+          <GoodsItem v-for="good in item.goods" :good="good" :key="good.id" />
+        </div>
       </div>
     </div>
   </div>
