6953aae768262ab5d593ead7a38072f13801d3a7
yigiuwole <3251013822@qq.com>
2026-03-31 12:48:56 +0800

    二级分类基础认识与路由配置

--- src/views/Home/components/HomeProduct.vue
+++ src/views/Home/components/HomeProduct.vue
@@ -8,7 +8,6 @@ import GoodsItem from './GoodsItem.vue'
 const goodsProduct = ref([])
 const getGoods = async () => {
   const res = await getGoodsAPI()
-  console.log(res);
   goodsProduct.value = res.result
 }
 
