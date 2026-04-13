2a5fb76a37015758d3c43950e3be86e92dfe33ca
yigiuwole <3251013822@qq.com>
2026-04-13 10:24:07 +0800

    订单页面点击切换地址弹出弹窗

--- src/views/Checkout/index.vue
+++ src/views/Checkout/index.vue
@@ -11,6 +11,8 @@ const getCheckout = async () => {
   curAddress.value = res.result.userAddresses.find((item) => item.isDefault === 0)
 }
 
+const toggleFlag = ref(false)
+
 onMounted(() => getCheckout())
 
 </script>
@@ -113,6 +115,23 @@ onMounted(() => getCheckout())
     </div>
   </div>
   <!-- 切换地址 -->
+  <el-dialog v-model="toggleFlag" title="切换收货地址" width="30%" center>
+    <div class="addressWrapper">
+      <div class="text item" v-for="item in checkInfo.userAddresses" :key="item.id">
+        <ul>
+          <li><span>收<i />货<i />人：</span>{{ item.receiver }} </li>
+          <li><span>联系方式：</span>{{ item.contact }}</li>
+          <li><span>收货地址：</span>{{ item.fullLocation + item.address }}</li>
+        </ul>
+      </div>
+    </div>
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button>取消</el-button>
+        <el-button type="primary">确定</el-button>
+      </span>
+    </template>
+  </el-dialog>
   <!-- 添加地址 -->
 </template>
 
